64d85063a7eb85235256eeae0bdccfcbf6dd16bc
madhurie57 <102281124+madhurie57@users.noreply.github.com>
2024-06-27 10:12:35 +0530

    Update resources.tf

--- resources.tf
+++ resources.tf
@@ -3,7 +3,7 @@ module "vnetNew" {
   version = "2.6.0"
   resource_group_name = "TerraformRG"
   vnet_location  = "East US"
-  vnet_name="<any_name>"
+  vnet_name="test1-vnet"
   address_space = ["180.0.0.0/16"]
   subnet_prefixes = ["180.0.1.0/24","180.0.2.0/24"]
   subnet_names = ["devsubnet","testsubnet"]
